5c2bc2478b94fcf227c3ba88481acbbc627f0145
kmarahar <kmarahar@myseneca.ca>
2023-05-01 15:21:30 +0530

    first commit

---
+++ frontend_react/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonials,
+  Works,
+} from "./container";
+
+import { Navbar } from "./components";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Works />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
