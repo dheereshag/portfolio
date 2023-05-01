dc52199bf77cc34f242ba98fca51fe163c4bac3d
dheereshagrwal <da11@iitbbs.ac.in>
2023-05-01 15:30:34 +0530

    working

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,18 @@ import "./Navbar.scss";
 import { images } from "../../constants";
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="Logo" />
       </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
