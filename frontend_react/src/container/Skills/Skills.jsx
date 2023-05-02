49469eb8be116d5a47e53224703ce035cc17c581
dheereshagrwal <da11@iitbbs.ac.in>
2023-05-02 16:44:35 +0530

    tooltip working for now but i don't like it

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,83 +1,77 @@
 import "./Skills.scss";
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import * as ReactTooltip from "react-tooltip";
+import { Tooltip } from "react-tooltip";
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 const Skills = () => {
-  const [skills, setSkills] = useState([]);
   const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
+
   useEffect(() => {
-    const skillsQuery = `*[_type == "skills"]`;
-    const experiencesQuery = `*[_type == "experiences"]`;
-    client.fetch(skillsQuery).then((skills) => {
-      setSkills(skills);
+    const experiencesQuery = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(experiencesQuery).then((data) => {
+      setExperiences(data);
     });
 
-    client.fetch(experiencesQuery).then((experiences) => {
-      setExperiences(experiences);
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
     });
   }, []);
+
   return (
     <>
-      <h2 className="head-text">Skills and Experience</h2>
+      <h2 className="head-text">Skills & Experiences</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills?.map((skill) => (
+          {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill?.name}
+              key={skill.name}
             >
               <div
                 className="app__flex"
-                style={{ backgroundColor: skill?.bgColor }}
+                style={{ backgroundColor: skill.bgColor }}
               >
-                <motion.img
-                  src={urlFor(skill?.icon)}
-                  alt={skill?.name}
-                  whileHover={{
-                    x: [0, -5, 5, -5, 5, 0],
-                    rotate: [-3, 3, -3, 3, -3],
-                    transition: {
-                      duration: 0.3,
-                      ease: "easeInOut",
-                      loop: Infinity,
-                    },
-                  }}
-                />
+                <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
-              <p className="p-text">{skill?.name}</p>
+              <p className="p-text">{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
-        <motion.div className="app__skills-exp">
-          {experiences?.works?.map((work) => (
-            <>
-              <motion.div
-                whileInView={{ opacity: [0, 1] }}
-                transition={{ duration: 0.5 }}
-                className="app__skills-exp-work"
-                data-tip
-                data-for={work?.name}
-                key={work?.name}
-              >
-                <h4 className="bold-text">{work?.name}</h4>
-                <p className="p-text">{work?.company}</p>
+        <div className="app__skills-exp">
+          {experiences?.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience?.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience?.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience?.works?.map((work, index) => (
+                  <motion.div
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="app__skills-exp-work"
+                    data-tooltip-content={work?.description}
+                    data-tooltip-id={`${work.name}-${index}`}
+                    key={`${work.name}-${index}`}
+                  >
+                    <h4 className="bold-text">{work?.name}</h4>
+                    <p className="p-text">{work?.company}</p>
+                    <Tooltip
+                      id={`${work.name}-${index}`}
+                      className="skills-tooltip"
+                    />
+                  </motion.div>
+                ))}
               </motion.div>
-              <ReactTooltip
-                id={work?.name}
-                effect="solid"
-                arrowColor="#fff"
-                className="skills-tooltip"
-              >
-                {work?.description}
-              </ReactTooltip>
-            </>
+            </motion.div>
           ))}
-        </motion.div>
+        </div>
       </div>
     </>
   );
