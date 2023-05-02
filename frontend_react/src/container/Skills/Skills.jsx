efed01674fd937dcf497ef8673cf1ea41561c74b
dheereshagrwal <da11@iitbbs.ac.in>
2023-05-02 17:48:40 +0530

    added shake

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,23 +1,28 @@
 import "./Skills.scss";
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { Tooltip } from "react-tooltip";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
 
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
-    const experiencesQuery = '*[_type == "experiences"]';
-    const skillsQuery = '*[_type == "skills"]';
+    const query = '*[_type == "experiences"]';
 
-    client.fetch(experiencesQuery).then((data) => {
+    client.fetch(query).then((data) => {
       setExperiences(data);
     });
+  }, []);
 
-    client.fetch(skillsQuery).then((data) => {
+  useEffect(() => {
+    const query = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
       setSkills(data);
     });
   }, []);
@@ -27,6 +32,7 @@ const Skills = () => {
       <h2 className="head-text">Skills & Experiences</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
+          {console.log("skills", skills)}
           {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
@@ -36,9 +42,17 @@ const Skills = () => {
             >
               <div
                 className="app__flex"
-                style={{ backgroundColor: skill.bgColor }}
+                style={{ backgroundColor: "var(--icon-background-color)" }}
               >
-                <img src={urlFor(skill.icon)} alt={skill.name} />
+                <motion.img
+                  src={urlFor(skill.icon)}
+                  alt={skill.name}
+                  whileHover={{
+                    scale: [1, 1.1, 1, 1.1, 1],
+                    rotate: [0, -2, 0, 2, 0],
+                    transition: { duration: 0.15, loop: Infinity },
+                  }}
+                />
               </div>
               <p className="p-text">{skill.name}</p>
             </motion.div>
@@ -52,21 +66,21 @@ const Skills = () => {
               </div>
               <motion.div className="app__skills-exp-works">
                 {experience?.works?.map((work, index) => (
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__skills-exp-work"
-                    data-tooltip-content={work?.description}
-                    data-tooltip-id={`${work.name}-${index}`}
+                  <Tippy
                     key={`${work.name}-${index}`}
+                    content={work?.description}
+                    animation="scale" // Use the scale animation
+                    duration={[500, 500]}
                   >
-                    <h4 className="bold-text">{work?.name}</h4>
-                    <p className="p-text">{work?.company}</p>
-                    <Tooltip
-                      id={`${work.name}-${index}`}
-                      className="skills-tooltip"
-                    />
-                  </motion.div>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                    >
+                      <h4 className="bold-text">{work?.name}</h4>
+                      <p className="p-text">{work?.company}</p>
+                    </motion.div>
+                  </Tippy>
                 ))}
               </motion.div>
             </motion.div>
@@ -77,4 +91,8 @@ const Skills = () => {
   );
 };
 
-export default AppWrap(Skills, "skills");
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__white-bg"
+);
--- frontend_react/src/container/Works/Works.jsx
+++ frontend_react/src/container/Works/Works.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Works.scss";
 
@@ -119,4 +119,8 @@ const Works = () => {
   );
 };
 
-export default AppWrap(Works, "work");
+export default AppWrap(
+  MotionWrap(Works, "app__works"),
+  "work",
+  "app__primary-bg"
+);
