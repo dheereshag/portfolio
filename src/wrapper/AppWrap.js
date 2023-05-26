a556642e9d6c68b7e96176a0b9b9bab7203d2daf
dheereshagrwal <da11@iitbbs.ac.in>
2023-05-26 10:02:18 +0530

    app__social removed

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavigationDots, SocialMedia,Footer } from "../components";
+import { NavigationDots, SocialMedia, Footer } from "../components";
 
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
       <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia classStyles={"app__social"}/>
+        <SocialMedia classStyles={"flex flex-col justify-end p-4 gap-2"} />
         <div className="app__wrapper app__flex">
           <Component />
           <Footer />
