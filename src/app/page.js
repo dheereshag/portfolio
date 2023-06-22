2099c3a93f4d082bf1eaebf3273a47ae217b2886
kmarahar <da11@iitbbs.ac.in>
2023-06-23 04:41:04 +0530

    removed unused imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import { Navbar } from "@/components";
 import { Header, About, Projects, Skills, Contact } from "@/container";
 import { AppProvider } from "@/context/AppContext";
-import Image from "next/image";
 
 export default function Home() {
   return (
