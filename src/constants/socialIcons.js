9507f0dd74fa06eb577a46497ec2bb0038c60039
kmarahar <da11@iitbbs.ac.in>
2023-06-23 14:10:14 +0530

    twitter

--- src/constants/socialIcons.js
+++ src/constants/socialIcons.js
@@ -2,7 +2,7 @@ const socialIcons = [
   { name: "github", href: "https://github.com/dheereshagrwal" },
   { name: "linkedin", href: "https://www.linkedin.com/in/dheereshagrwal" },
   { name: "leetcode", href: "https://leetcode.com/dheereshagrwal/" },
-  { name: "instagram", href: "https://instagram.com/dheereshagrwal" },
+  { name: "twitter", href: "https://twitter.com/dheereshagrwal" },
 ];
 
 export default socialIcons;
