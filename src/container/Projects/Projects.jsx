ff689f786bfbb3ae1783bebb50594199469cdd1a
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-08-21 21:17:36 +0530

    unused

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "@/client";
 
