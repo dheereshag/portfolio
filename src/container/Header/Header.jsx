a33915c432271a775c19d59ae98fb0aba3de70a9
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-08-25 18:50:46 +0530

    new

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,99 +1,44 @@
 "use client";
-import React, { useRef, useEffect, useContext, useState } from "react";
+import Link from "next/link";
 import { AppWrap } from "../../wrapper";
-import { motion } from "framer-motion";
-import "./Header.scss";
-import headerIcons from "../../constants/headerIcons";
-import Tippy from "@tippyjs/react";
-import "tippy.js/dist/tippy.css";
-import "tippy.js/animations/scale.css";
+
 const Header = () => {
-  const [isTippyLoaded, setIsTippyLoaded] = useState(false);
-  const dragConstraints = {
-    top: -100,
-    left: -100,
-    right: 100,
-    bottom: 100,
-  };
   return (
-    <div
-      id="home"
-      className="flex flex-col lg:flex-row gap-4 mt-4 lg:gap-0 xl:mr-64"
-    >
-      <div className="animate__animated animate__fadeInLeft animate__delay-1s">
-        <div className="items-start flex flex-col xl:items-end gap-8">
-          <div className="flex gap-8 shadow rounded-2xl px-4 py-3 items-center">
-            <span className="text-5xl">👋🏻</span>
-            <div className="font-dm-sans flex flex-col gap-2">
-              <p className="">Hello, I am</p>
-              <p className="text-violet-800 font-semibold text-3xl">Dheeresh</p>
-            </div>
-          </div>
-          <div className="flex flex-col py-3 px-14 border shadow font-poppins rounded-2xl text-right gap-1 text-sm">
-            <p className="">Full-Stack Developer</p>
-          </div>
+    <div className="flex justify-evenly" id="home">
+      <aside className="flex flex-col gap-4 p-4 font-dm-sans w-5/12 rounded-xl shadow-md ">
+        <h1 className="text-4xl font-extrabold">
+          I'm Dheeresh. I am a web developer.
+        </h1>
+        <p>
+          I've loved making things for as long as I can remember, and wrote my
+          first program when I was 6 years old, just two weeks after my mom
+          brought home the brand new Macintosh LC 550 that I taught myself to
+          type on.
+        </p>
+        <p>
+          I spent the next few summers indoors working on a rocket design, while
+          I recovered from the multiple surgeries it took to fix my badly broken
+          legs. It took nine iterations, but when I was 15 I sent my dad's
+          Blackberry into orbit and was able to transmit a photo back down to
+          our family computer from space.
+        </p>
+      </aside>
+      <aside className="flex flex-col">
+        <img
+          src="./square.jpg"
+          alt="profile_bg"
+          className="grayscale hover:grayscale-0 bg-cover bg-grey-300 w-96 shadow rounded-2xl transform rotate-3 scale-110"
+        />
+        <div className="mt-10">
+          <Link
+            className="flex items-center gap-2 cursor-pointer hover:text-green-500"
+            href="https://github.com/dheereshagrwal"
+          >
+            <i className="ci ci-github"></i>
+            <p>Github</p>
+          </Link>
         </div>
-      </div>
-
-      <div className="flex flex-col lg:flex-row gap-8">
-        <div className="flex relative">
-          <img
-            src="./profile.png"
-            alt="profile_bg"
-            className="z-10 animate__animated animate__fadeIn animate__slow"
-          />
-          <img
-            className="absolute bottom-2 animate__animated animate__zoomIn animate__slow"
-            src="./circle.svg"
-            alt="profile_circle"
-          />
-        </div>
-        <Tippy
-          content={`if you want to play around, drag the icons`}
-          animation="scale" // Use the scale animation
-          duration={[500, 500]}
-          placement="right"
-          arrow={true}
-          delay={isTippyLoaded ? [3000, 0] : [0, 0]}
-          onShown={() => setIsTippyLoaded(true)}
-          className="font-karla border shadow-lg px-4 py-3"
-        >
-          <div className="flex lg:flex-col justify-evenly flex-wrap animate__animated animate__zoomIn animate__delay-1s animate__slow">
-            <motion.div
-              className="app__flex rounded-full shadow-md bg-white w-40 h-40 border"
-              key={`circle-0`}
-              drag
-              dragConstraints={dragConstraints}
-            >
-              <i
-                className={`ci ci-${headerIcons[0].name} ci-${headerIcons[0].size}`}
-              ></i>
-            </motion.div>
-
-            <motion.div
-              className="app__flex rounded-full shadow-md bg-white w-20 h-20 border"
-              key={`circle-1`}
-              drag
-              dragConstraints={dragConstraints}
-            >
-              <i
-                className={`ci ci-${headerIcons[1].name} ci-${headerIcons[1].size}`}
-              ></i>
-            </motion.div>
-
-            <motion.div
-              className="app__flex rounded-full shadow-md bg-white w-28 h-28 border"
-              key={`circle-2`}
-              drag
-              dragConstraints={dragConstraints}
-            >
-              <i
-                className={`ci ci-${headerIcons[2].name} ci-${headerIcons[2].size}`}
-              ></i>
-            </motion.div>
-          </div>
-        </Tippy>
-      </div>
+      </aside>
     </div>
   );
 };
