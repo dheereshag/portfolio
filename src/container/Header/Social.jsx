1a04374f72e480a2a0c8df3c209bef57028ffd42
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-09-01 21:00:33 +0530

    header and social

---
+++ src/container/Header/Social.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+const Social = () => {
+  return (
+    <div className="gap-16 flex flex-col ">
+      <img
+        src="./square.jpg"
+        alt="profile_bg"
+        className="hover:grayscale bg-cover bg-grey-300 shadow rounded-2xl transform rotate-3 scale-110 mr"
+      />
+
+      <div className="grid grid-cols-2 gap-4 font-inter text-zinc-300">
+        <Link
+          className="flex items-center gap-4 cursor-pointer hover:text-blue-500"
+          href="mailto:da11@iitbbs.ac.in"
+        >
+          <i class="fa-solid fa-envelope"></i>
+          <p>da11@iitbbs.ac.in</p>
+        </Link>
+        <Link
+          className="flex items-center gap-4 cursor-pointer hover:text-blue-500"
+          href="https://github.com/dheereshagrwal"
+          target="_blank"
+        >
+          <i className="ci ci-lg ci-github ci-invert"></i>
+          <p>Github</p>
+        </Link>
+
+        <Link
+          className="flex items-center gap-4 cursor-pointer hover:text-blue-500"
+          href="https://www.linkedin.com/in/dheereshagrwal/"
+          target="_blank"
+        >
+          <i className="ci ci-lg ci-linkedin"></i>
+          <p>LinkedIn </p>
+        </Link>
+
+        <Link
+          className="flex items-center gap-4 cursor-pointer hover:text-blue-500"
+          href="https://leetcode.com/dheereshagrwal/"
+          target="_blank"
+        >
+          <i className="ci ci-lg ci-leetcode-light"></i>
+          <p>Leetcode </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Social;
