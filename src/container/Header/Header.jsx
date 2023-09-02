2ac110f54378e86a7a940e7c2bf28858e50ab2c1
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-09-02 21:44:08 +0530

    cv button

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -15,14 +15,14 @@ const Header = () => {
         {/* button to view CV */}
         <Link
           href="https://drive.google.com/file/d/1B32Dcsmr-1fQStcyyvHlbJ0RCNBjdOx0/view?usp=sharing"
-          className="hvr-shrink w-64 mt-8"
+          className="hvr-shrink mt-8"
           target="_blank"
           rel="noreferrer"
         >
-          <li className="bg-black text-white mt-4 text-xl transform-gpu font-bold py-2 px-4 hover:rounded-full w-8/12 font-poppins flex items-center justify-between">
+          <div className="bg-black text-white mt-4 text-xl font-bold py-2 px-4 w-40 hover:rounded-full font-poppins flex items-center gap-4">
             <span>View CV</span>
             <i className="fa-solid fa-eye text-white"></i>
-          </li>
+          </div>
         </Link>
       </aside>
       <aside className="xl:mr-28 lg:mr-10 mt-16 w-full md:w-7/12 lg:w-5/12 xl:w-3/12">
