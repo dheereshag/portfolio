ab74be347df4b5a8b1a5c0a1f1ffb59fb431da83
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-09-03 01:57:06 +0530

    loader for projects

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -16,6 +16,8 @@ const Projects = () => {
   const [animateCard, setAnimateCard] = useState({ y: [null, 0], opacity: 1 });
   const [filterProjects, setFilterProjects] = useState([]);
   const [projects, setProjects] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     async function fetchProjects() {
       try {
@@ -25,7 +27,7 @@ const Projects = () => {
         const data = await response.json();
         setProjects(data);
         setFilterProjects(data);
-        console.log(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching skill data:", error);
       }
@@ -55,172 +57,108 @@ const Projects = () => {
       <h2 className="font-dm-sans text-4xl xl:text-5xl font-semibold text-zinc-400 text-center leading-tight">
         My <span className="text-white">Projects</span> section
       </h2>
-      <div className="flex flex-col items-center gap-10 xl:gap-16">
-        <section className="gap-6 flex flex-wrap justify-center">
-          {items.map((item, index) => (
-            <motion.button
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`px-5 py-3 font-sora font-semibold text-sm text-zinc-300 ${
-                activeFilter === item
-                  ? "rounded-none bg-black"
-                  : "rounded-xl bg-zinc-800"
-              }`}
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
+      {isLoading ? (
+        <div className="text-white">Loading...</div>
+      ) : (
+        <div className="flex flex-col items-center gap-10 xl:gap-16">
+          <section className="gap-6 flex flex-wrap justify-center">
+            {items.map((item, index) => (
+              <motion.button
+                key={index}
+                onClick={() => handleWorkFilter(item)}
+                className={`px-5 py-3 font-sora font-semibold text-sm text-zinc-300 ${
+                  activeFilter === item
+                    ? "rounded-none bg-black"
+                    : "rounded-xl bg-zinc-800"
+                }`}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <div className="flex items-center gap-1">
+                  <span>{item.name}</span>
+                  {item.name !== "all" && (
+                    <i
+                      className={`ci ci-${item.name} ci-${item.iconStyle}`}
+                    ></i>
+                  )}
+                </div>
+              </motion.button>
+            ))}
+          </section>
+
+          <section>
+            <motion.div
+              animate={animateCard}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="flex flex-wrap justify-center gap-8 lg:gap-14"
             >
-              <div className="flex items-center gap-1">
-                <span>{item.name}</span>
-                {item.name !== "all" && (
-                  <i className={`ci ci-${item.name} ci-${item.iconStyle}`}></i>
-                )}
-              </div>
-            </motion.button>
-          ))}
-        </section>
-        {/*         
-        <section>
-          <motion.div
-            animate={animateCard}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="flex flex-wrap justify-center gap-8 lg:gap-14"
-          >
-            {filterProjects.map((project, index) => {
-              return (
-                <motion.div
-                  className="hvr-grow-shadow md:w-96 p-3 bg-zinc-800 rounded-3xl"
-                  key={`${project.title}-${index}`}
-                >
-                  <div className="flex relative">
-                    <img
-                      src={urlFor(project.image)}
-                      alt={project.title}
-                      className="object-contain rounded-xl"
-                    />
+              {filterProjects.map((project, index) => {
+                return (
+                  <motion.div
+                    className="hvr-grow-shadow md:w-96 p-3 bg-zinc-800 rounded-3xl"
+                    key={`${project.name}`}
+                  >
+                    <div className="flex relative">
+                      <img
+                        src={project.imageUri}
+                        alt={project.name}
+                        className="object-contain rounded-xl"
+                      />
 
-                    <motion.div
-                      transition={{
-                        duration: 0.25,
-                        type: "tween",
-                        staggerChildren: 0.5,
-                      }}
-                      className="group/dark flex justify-center items-center absolute h-full w-full hover:bg-black/50 gap-8 rounded-xl"
-                    >
-                      <a
-                        href={project?.projectLink}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
+                      <motion.div
+                        transition={{
+                          duration: 0.25,
+                          type: "tween",
+                          staggerChildren: 0.5,
+                        }}
+                        className="group/dark flex justify-center items-center absolute h-full w-full hover:bg-black/50 gap-8 rounded-xl"
                       >
-                        <i className="fa-solid fa-eye fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
-                      </a>
-                      <a
-                        href={project?.codeLink}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
-                      >
-                        <i className="fa-brands fa-github fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
-                      </a>
-                    </motion.div>
-                  </div>
-                  <div className="flex justify-center py-1 relative">
-                    <div className="flex items-center px-2 py-2 -top-5 bg-zinc-800 rounded-xl absolute">
-                      <div className="flex items-center gap-1.5">
-                        {project.icons.map((icon) => (
-                          <i className={`ci ci-${icon.icon} ${icon.size}`}></i>
-                        ))}
+                        <a
+                          href={project?.liveLink}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
+                        >
+                          <i className="fa-solid fa-eye fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
+                        </a>
+                        <a
+                          href={project?.githubLink}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
+                        >
+                          <i className="fa-brands fa-github fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
+                        </a>
+                      </motion.div>
+                    </div>
+                    <div className="flex justify-center py-1 relative">
+                      <div className="flex items-center px-2 py-2 -top-5 bg-zinc-800 rounded-xl absolute">
+                        <div className="flex items-center gap-1.5">
+                          {project.techStacks.map((techStack) => (
+                            <i
+                              className={`ci ci-${techStack.icon.iconName} ci-${techStack.icon.iconStyle}`}
+                            ></i>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="flex relative mt-2">
+                        <aside className="flex flex-col gap-2">
+                          <h4 className="font-sora font-semibold text-white">
+                            {project?.name}
+                          </h4>
+                          <p className="font-dm-sans break-words text-sm text-zinc-300">
+                            {project?.description}
+                          </p>
+                        </aside>
                       </div>
                     </div>
-                    <div className="flex relative mt-2">
-                      <aside className="flex flex-col gap-2">
-                        <h4 className="font-sora font-semibold text-white">
-                          {project?.title}
-                        </h4>
-                        <p className="font-dm-sans break-words text-sm text-zinc-300">
-                          {project?.description}
-                        </p>
-                      </aside>
-                    </div>
-                  </div>
-                </motion.div>
-              );
-            })}
-          </motion.div>
-        </section> */}
-
-        <section>
-          <motion.div
-            animate={animateCard}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="flex flex-wrap justify-center gap-8 lg:gap-14"
-          >
-            {filterProjects.map((project, index) => {
-              return (
-                <motion.div
-                  className="hvr-grow-shadow md:w-96 p-3 bg-zinc-800 rounded-3xl"
-                  key={`${project.name}`}
-                >
-                  <div className="flex relative">
-                    <img
-                      src={project.imageUri}
-                      alt={project.name}
-                      className="object-contain rounded-xl"
-                    />
-
-                    <motion.div
-                      transition={{
-                        duration: 0.25,
-                        type: "tween",
-                        staggerChildren: 0.5,
-                      }}
-                      className="group/dark flex justify-center items-center absolute h-full w-full hover:bg-black/50 gap-8 rounded-xl"
-                    >
-                      <a
-                        href={project?.liveLink}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
-                      >
-                        <i className="fa-solid fa-eye fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
-                      </a>
-                      <a
-                        href={project?.githubLink}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="group/icon app__flex w-12 h-12 bg-black/50 rounded-full opacity-0 group-hover/dark:opacity-100 transition-all"
-                      >
-                        <i className="fa-brands fa-github fa-xl text-white transition-transform duration-300 transform-gpu group-hover/icon:scale-75"></i>
-                      </a>
-                    </motion.div>
-                  </div>
-                  <div className="flex justify-center py-1 relative">
-                    <div className="flex items-center px-2 py-2 -top-5 bg-zinc-800 rounded-xl absolute">
-                      <div className="flex items-center gap-1.5">
-                        {project.techStacks.map((techStack) => (
-                          <i
-                            className={`ci ci-${techStack.icon.iconName} ci-${techStack.icon.iconStyle}`}
-                          ></i>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="flex relative mt-2">
-                      <aside className="flex flex-col gap-2">
-                        <h4 className="font-sora font-semibold text-white">
-                          {project?.name}
-                        </h4>
-                        <p className="font-dm-sans break-words text-sm text-zinc-300">
-                          {project?.description}
-                        </p>
-                      </aside>
-                    </div>
-                  </div>
-                </motion.div>
-              );
-            })}
-          </motion.div>
-        </section>
-      </div>
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          </section>
+        </div>
+      )}
     </div>
   );
 };
