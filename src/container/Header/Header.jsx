82e82859acb586d16b7731483be0f01770357390
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-09-02 19:34:53 +0530

    header responsive

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,14 +6,14 @@ import Social from "./Social";
 const Header = () => {
   return (
     <div
-      className="flex flex-col md:flex-row justify-between text-zinc-300"
+      className="flex flex-col lg:flex-row justify-between text-zinc-300"
       id="home"
     >
-      <aside className="flex flex-col gap-4 mt-16 md:m-10 font-dm-sans w-full md:w-5/12">
+      <aside className="flex flex-col gap-4 mt-16 lg:m-10 font-dm-sans w-full lg:w-5/12">
         <HeaderText />
       </aside>
 
-      <aside className="mr-28 mt-16 w-full md:w-3/12">
+      <aside className="lg:mr-28 mt-16 w-full md:w-7/12 lg:w-3/12">
         <Social />
       </aside>
     </div>
