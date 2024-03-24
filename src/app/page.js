8a62571e7493c9c31df9cfe77242914154b6f39e
Dheeresh Agarwal <da11@iitbbs.ac.in>
2024-03-24 15:57:15 +0530

    portfolio down

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 import { Navbar } from "../components";
-import { Header, Projects, Skills, Contact } from "../container"
+import { Header, Projects, Skills, Contact } from "../container";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <Skills />
-      <Projects />
-      <Contact />
-    </>
+    <div className="flex justify-center items-center h-screen bg-stone-100">
+      <h1 className="text-3xl font-inter">working on a better portfolio 😭</h1>
+    </div>
+
+    // <>
+    //   <Navbar />
+    //   <Header />
+    //   <Skills />
+    //   <Projects />
+    //   <Contact />
+    // </>
   );
 }
