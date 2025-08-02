5a8e972c4a84cac93791da85642f5c760eeb4584
Dheeresh Agarwal <da11@iitbbs.ac.in>
2025-08-02 11:49:30 +0530

    remove analytics and favicon setup for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,6 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: "/site.webmanifest",
   category: "portfolio",
   classification: "Personal Portfolio Website",
   referrer: "origin-when-cross-origin",
